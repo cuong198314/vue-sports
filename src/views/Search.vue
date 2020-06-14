8d97197261f4b3f52f4681a6f133846e1a6dacd9
cuong198314 <maic@seattleu.edu>
2020-06-13 20:11:29 -0700

    in progress

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,23 +1,102 @@
 <template>
-  <div class="Search">
-    <h1>This is the search page</h1>
-    <h2>Please enter your search query!</h2>
-    <label>Query:
-      <input type="text" id="Query" v-model="message">
-    </label>
-    <h2>Your search results:</h2>
+  <div class="Sunrise-Sunset">
+    <h1>Sunrise-Sunset</h1>
+   
+  
+
+    <form v-on:submit.prevent="findCity">
+    <p>Enter city name: <input type="text" v-model="query" placeholder="Seattle, WA"> <button type="submit">Go</button></p>
+    
+      </form>
+
+       <load-spinner v-if="showLoading"></load-spinner>
+    <ul class="cities" v-if="results && results.list.length > 0">
+     <li v-for="(city,index) in results.list" :key="index">
+        <h2>{{ city.name }}, {{ city.sys.country }}</h2>
+        
+
+       
+
+        <p><button class="save" v-on:click="saveCity(city)">Save City to Favorites</button></p>
+      </li>
+    </ul>
+
    
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'Search',
+
   data() {
     return {
-      message: ''
+       results: null,
+      query: '',
+      showLoading: false,
+      messages: [],
+      favorites: []
+    }
+  },
+
+   methods: {
+
+    
+    getCity: function () {
+      this.showSpinner = true
+ 
+     console.log ()
+     
+      
+     .get("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today", {
+        
+        params: {
+         lat: '36.7201600&lng=-4.4203400'
+        }
+        }
+        )
+        .then(response => {
+         
+          this.results = response.data;
+          this.showLoading = false;
+        })
+        .catch(error => {
+          this.messages.push({
+            type: 'error',
+            text: error.message
+          });
+         
+        });
+      }
     }
   }
  
-}
+
 </script>
+<style scoped>
+.errors li {
+  color: red;
+  border: solid red 1px;
+  padding: 5px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  width: 300px;
+  min-height: 300px;
+  border: solid 1px #e8e8e8;
+  padding: 10px;
+  margin: 5px;
+}
+a {
+  color: #42b983;
+}
+</style>
